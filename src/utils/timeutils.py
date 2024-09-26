15d34cd4e86e5e59428f542de3b81fb92341bc2a
ZhongFuze <zella@mask.io>
2024-09-27 00:11:53 +0800

    [#] timeutils

--- src/utils/timeutils.py
+++ src/utils/timeutils.py
@@ -4,7 +4,7 @@
 Author: Zella Zhong
 Date: 2024-09-12 19:13:50
 LastEditors: Zella Zhong
-LastEditTime: 2024-09-14 22:41:11
+LastEditTime: 2024-09-26 21:11:24
 FilePath: /data_process/src/utils/timeutils.py
 Description: 
 '''
@@ -26,3 +26,11 @@ def iso8601_string_to_datetime(value):
     dt = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%fZ")
     formatted_date = dt.strftime("%Y-%m-%d %H:%M:%S")
     return formatted_date
+
+
+def get_unix_milliconds():
+    # Get the current time in seconds since the Epoch
+    current_time_seconds = time.time()
+    # Convert to milliconds by multiplying by 1e6 and converting to an integer
+    milliconds = int(current_time_seconds * 1e6)
+    return milliconds
