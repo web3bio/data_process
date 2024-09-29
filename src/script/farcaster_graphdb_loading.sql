e0e28dda713aaa8eaf72ea6d3554ec0227c1877e
ZhongFuze <zella@mask.io>
2024-09-30 02:22:01 +0800

    [!] fix change dirs

--- src/script/farcaster_graphdb_loading.sql
+++ src/script/farcaster_graphdb_loading.sql
@@ -1,11 +1,11 @@
 CREATE LOADING JOB Load_Ens FOR GRAPH SocialGraph {
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/Identities.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/farcaster/Identities.csv"
     TO VERTEX Identities VALUES($"primary_id", $"primary_id", $"platform", $"identity", $"update_time") USING SEPARATOR="\t", HEADER="true", EOL="\n";
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/IdentitiesGraph.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/farcaster/IdentitiesGraph.csv"
     TO VERTEX IdentitiesGraph VALUES($"primary_id", $"primary_id", $"updated_nanosecond") USING SEPARATOR="\t", HEADER="true", EOL="\n";
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/PartOfIdentitiesGraph.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/farcaster/PartOfIdentitiesGraph.csv"
     TO EDGE PartOfIdentitiesGraph VALUES ($"from", $"to") USING SEPARATOR = "\t", EOL = "\n", HEADER = "true";
 
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/Hold.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/farcaster/Hold.csv"
     TO EDGE Hold VALUES ($"from", $"to", $"source", $"level") USING SEPARATOR = "\t", EOL = "\n", HEADER = "true";
 }
--- src/script/lens_graphdb_loading.sql
+++ src/script/lens_graphdb_loading.sql
@@ -1,15 +1,15 @@
 CREATE LOADING JOB Load_Ens FOR GRAPH SocialGraph {
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/Identities.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/lens/Identities.csv"
     TO VERTEX Identities VALUES($"primary_id", $"primary_id", $"platform", $"identity", $"update_time") USING SEPARATOR="\t", HEADER="true", EOL="\n";
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/IdentitiesGraph.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/lens/IdentitiesGraph.csv"
     TO VERTEX IdentitiesGraph VALUES($"primary_id", $"primary_id", $"updated_nanosecond") USING SEPARATOR="\t", HEADER="true", EOL="\n";
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/PartOfIdentitiesGraph.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/lens/PartOfIdentitiesGraph.csv"
     TO EDGE PartOfIdentitiesGraph VALUES ($"from", $"to") USING SEPARATOR = "\t", EOL = "\n", HEADER = "true";
 
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/Hold.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/lens/Hold.csv"
     TO EDGE Hold VALUES ($"from", $"to", $"source", $"level") USING SEPARATOR = "\t", EOL = "\n", HEADER = "true";
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/Resolve.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/lens/Resolve.csv"
     TO EDGE Resolve VALUES ($"from", $"to", $"source", $"level") USING SEPARATOR = "\t", EOL = "\n", HEADER = "true";
-  LOAD "/home/tigergraph/shared_data/import_graphs/ensname/Reverse_Resolve.csv"
+  LOAD "/home/tigergraph/shared_data/import_graphs/lens/Reverse_Resolve.csv"
     TO EDGE Reverse_Resolve VALUES ($"from", $"to", $"source", $"level") USING SEPARATOR = "\t", EOL = "\n", HEADER = "true";
 }
